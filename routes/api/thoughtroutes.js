b013e1f8b359271815d5313376340f368e05ec21
Lance Nguyen <lnguyen1275@gmail.comgit config --global user.name Lance>
2023-09-06 02:45:24 -0500

    fixed routes and tested

--- routes/api/thoughtroutes.js
+++ routes/api/thoughtroutes.js
@@ -7,7 +7,7 @@ const {
   deleteThought,
   addReaction,
   removeReaction
-} = require('../../controllers/thoughtController')
+} = require('../../controllers/thoughtscontroller')
 
 
 router.route('/')
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -7,7 +7,7 @@ const {
   deleteUser,
   addFriend,
   removeFriend
-} = require('../../controllers/userController')
+} = require('../../controllers/usercontroll')
 
 
 router.route('/')
